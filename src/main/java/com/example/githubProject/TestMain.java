52797e9de3c400d31acb809b90e68074d0e97352
gopi201 <gopi10.0693@gmail.com>
2021-09-15 13:26:59 +0530

    initial commit

---
+++ src/main/java/com/example/githubProject/TestMain.java
@@ -0,0 +1,8 @@
+package com.example.githubProject;
+
+public class TestMain {
+
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
---
+++ src/test/java/com/example/githubProject/GithubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
